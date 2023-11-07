3cae7090339099b6113cc3008d1efab21dcb502a
Vincent Ngan <ngan.vincent@gmail.com>
2023-11-06 22:48:11 -0500

    Added API for accessing Category data.

--- backend/routers/ExpressApiRouter.ts
+++ backend/routers/ExpressApiRouter.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import {AddProductCommand, UpdateProductCommand} from "../../common/usecases";
+import {AddCategoryCommand, AddProductCommand, UpdateProductCommand} from "../../common/usecases";
 import {UseCaseInteractors} from "../interactors";
 
 export function ExpressApiRouter(interactors: UseCaseInteractors): Router {
@@ -58,6 +58,54 @@ export function ExpressApiRouter(interactors: UseCaseInteractors): Router {
 
     router.delete('/products', (req, res) => {
         interactors.DeleteAllProducts.execute({})
+            .then(() => res.status(200).json())
+            .catch(error => res.status(400).json({error: error.message}));
+    });
+
+    router.get('/categories', (req, res, next) => {
+        const name = req.query["name"] as string | undefined
+        if (name !== undefined) {
+            interactors.GetCategoryByName.execute({name: name})
+                .then(category => res.status(200).json(category))
+                .catch(error => res.status(400).json({error: error.message}))
+        } else next()
+    })
+
+    router.get('/categories', (req, res) => {
+        interactors.GetAllCategories.execute({})
+            .then(categories => res.status(200).json(categories))
+            .catch(error => res.status(400).json({error: error.message}))
+    })
+
+    router.get('/categories/:id', (req, res) => {
+        const id = req.params.id;
+        interactors.GetCategory.execute({id: id})
+            .then(category => {
+                if (category === undefined || category === null) {
+                    res.status(404).json({error: `Category with id ${id} not found`});
+                } else {
+                    res.status(200).json(category);
+                }
+            })
+            .catch(error => res.status(400).json({error: error.message}));
+    });
+
+    router.post<any, any, any, AddCategoryCommand>('/categories', (req, res) => {
+        const request = req.body
+        interactors.AddCategory.execute(request)
+            .then(() => res.status(201).json())
+            .catch(error => res.status(400).json({error: error.message}));
+    });
+
+    router.delete('/categories/:id', (req, res) => {
+        const id = req.params.id;
+        interactors.DeleteCategory.execute({id: id})
+            .then(() => res.status(200).json())
+            .catch(error => res.status(400).json({error: error.message}));
+    });
+
+    router.delete('/categories', (req, res) => {
+        interactors.DeleteAllCategories.execute({})
             .then(() => res.status(200).json())
             .catch(error => res.status(400).json({error: error.message}));
     });
